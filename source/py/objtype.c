cd2c83ff79b67c1fce1dc010820fc19233b77555
Kevin Thomas <kevin@mytechnotalent.com>
2020-09-29 20:09:57 -0400

    Update MiniPython references

--- source/py/objtype.c
+++ source/py/objtype.c
@@ -3,8 +3,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2020 Kevin Thomas
- * Copyright (c) 2014-2016 Paul Sokolovsky
+ * Copyright (c) 2020 My Techno Talent, LLC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
